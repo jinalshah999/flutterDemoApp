15f6d48884531de0344a4a17e3217df1427f79e7
jinalshah999 <jinalshah999@gmail.com>
2019-10-11 11:41:11 +0530

    added counter

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,48 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Text(
-          'Hello World',
-          style: TextStyle(
-            fontSize: 30.0,
-            fontWeight: FontWeight.bold,
+      home: MyAppHomePage(),
+    );
+  }
+}
+
+class MyAppHomePage extends StatefulWidget {
+  @override
+  _MyAppHomePage createState() {
+    // TODO: implement createState
+    return _MyAppHomePage();
+  }
+}
+
+class _MyAppHomePage extends State<MyAppHomePage> {
+  int _counter = 0;
+void incCounter(){
+ setState(() {
+  _counter=_counter+1; 
+ });
+ 
+}
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Factorial Demo'),
+      ),
+      body: Column(
+        children: <Widget>[
+          RaisedButton(
+            child: Text('Click Me1'),
+            onPressed: incCounter,
           ),
-        ),
+          Text(
+            'You pressed Button $_counter',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
       ),
     );
   }
